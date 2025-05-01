a2a7b16d918121321ee3e2981124136a56aac435
Delgado_C <delgadocharles1614@gmail.com>
2025-05-01 23:57:18 +0800

    commit

--- Trash/main.js
+++ Trash/main.js
@@ -10,14 +10,14 @@ $(document).ready(function () {
     $(".filter-item").click(function () {
         const value = $(this).attr("data-filter");
         if (value == "all") {
-            $(".post-box").show("1000");
+            $(".post-box").show("800");
         } else {
             $(".post-box")
                 .not("." + value)
-                .hide(1000);
+                .hide(800);
             $(".post-box")
                 .filter("." + value)
-                .show("1000");
+                .show("800");
         }
     });
     $(".filter-item").click(function () {
